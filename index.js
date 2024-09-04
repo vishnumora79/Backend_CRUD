dab4270b3e1b3826d74225fd09fbf7ec781d1623
vishnu mora <moravishnu2005@gmail.com>
2024-09-04 07:00:50 +0530

    Simple CRUD application using Node with Express framework

---
+++ index.js
@@ -0,0 +1,87 @@
+import express, { request, response } from 'express'
+
+const app = express()
+
+const port = 3000
+
+// Middleware
+app.use(express.json());
+
+
+let teaData = []
+let nextId = 1;
+
+// Add a new tea into an array
+app.post("/teas", (request, response) => {
+    const {name, price} = request.body;
+    const newData = {id: nextId++, name, price};
+    teaData.push(newData);
+    response.status(201);
+    response.send(newData);
+
+    console.log(request.body);
+});
+
+
+// Get all teas
+app.get("/teas", (request, response) => {
+    response.status(200).send(teaData);
+});
+
+// Get a spectific tea
+app.get("/teas/:id", (request, response) => {
+   const tea = teaData.find(t => t.id === parseInt(request.params.id));
+//    console.log(tea);
+//    console.log(teaData);
+   if(!tea) {
+    response.status(404).send("Tea  not found!");
+   }
+
+   response.status(200).send(tea);
+})
+
+// Updating the data
+
+app.put("/teas/:id", (request, response) => {
+  const tea = teaData.find(t => t.id === parseInt(request.params.id));
+  
+  if(!tea) {
+    return response.status(404).send("Tea  not found");
+  }
+  const {name, price} = request.body;
+  tea.name = name;
+  tea.price = price;
+  response.status(200).send(tea);
+});
+
+
+// Deleting data
+
+app.delete("/teas/:id", (request, response) => {
+   const index = teaData.findIndex(t => t.id === parseInt(request.params.id));
+   if(index == -1) {
+    return response.status(404).send("tea not found");
+   }
+
+   teaData.splice(index, 1);
+   return response.status(204).send("Deleted");
+});
+
+// app.get("/", (request, response) => {
+//   response.send("Hello from vishnu nad his super man");
+// });
+
+
+// app.get("/ice-tea", (request, response) => {
+//   response.send("Can you ask me, how are you man?");
+// });
+
+// app.get("/x", (request, response) => {
+//    response.send("vishnu_mora");
+// });
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}...`)
+})
